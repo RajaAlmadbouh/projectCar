f3a40e9fdc28e0c3e97cc476d71a0c388f4b534c
RajaAlmadbouh <146444351+RajaAlmadbouh@users.noreply.github.com>
2023-11-14 09:06:53 +0300

    fuel Economy

--- JS/theAccounts.js
+++ JS/theAccounts.js
@@ -1,27 +1,53 @@
-/*var fuelEconomy = parseFloat(document.getElementById('fuelEconomy'));
-var distanceTraveld = parseFloat(document.getElementById('distanceTraveld').value);
+var fuelEconomy = document.getElementById('fuelEconomyForm');
+/*var distanceTraveld = parseFloat(document.getElementById('distanceTraveld').value);
 var carFuel = parseFloat(document.getElementById('carFuel').value);
 var priceOfGasoline = parseFloat(document.getElementById('priceOfGasoline').value);*/
 
 //console.log(priceOfGasoline);
 
-function calculateFuelConsumption() {
-    // Get input values
-    var distanceTraveled = parseFloat(document.getElementById("distanceTraveled").value);
-    var carFuel = parseFloat(document.getElementById("carFuel").value);
-    var priceOfGasoline = parseFloat(document.getElementById("priceOfGasoline").value);
+fuelEconomy.addEventListener("submit", function(e){
+    e.preventDefault();
+
+    var distanceTraveled = document.getElementById("distanceTraveld");
+    var carFuel = document.getElementById("carFuel");
+    var priceOfGasoline = document.getElementById("priceOfGasoline");
 
     // Check if inputs are valid numbers
-    if (isNaN(distanceTraveled) || isNaN(carFuel) || isNaN(priceOfGasoline)) {
+    if (isNaN(distanceTraveled.value) || isNaN(carFuel.value) || isNaN(priceOfGasoline.value)) {
         alert("Please enter valid numbers for all fields.");
         return;
     }
 
     // Perform calculations
-    var vDistanceTraveled = distanceTraveled / 100.0;
-    var carFuelPrice = carFuel * priceOfGasoline;
+    var vDistanceTraveled = distanceTraveled.value / 100.0;
+    var carFuelPrice = carFuel.value * priceOfGasoline.value;
     var fuelConsumption = vDistanceTraveled * carFuelPrice;
 
     // Display the result
-    document.getElementById("fuelConsumptionResult").innerHTML = "Amount of fuel consumption in Jordanian dinars JD: " + fuelConsumption + " JD.";
-}
+    document.getElementById("fuelConsumption").innerHTML = "Amount of fuel consumption in Jordanian dinars JD: " + fuelConsumption + " JD.";
+
+
+});
+
+/*function calculateFuelConsumption() {
+
+
+    // Get input values
+    var distanceTraveled = document.getElementById("distanceTraveld");
+    var carFuel = document.getElementById("carFuel");
+    var priceOfGasoline = document.getElementById("priceOfGasoline");
+
+    // Check if inputs are valid numbers
+    if (isNaN(distanceTraveled.value) || isNaN(carFuel.value) || isNaN(priceOfGasoline.value)) {
+        alert("Please enter valid numbers for all fields.");
+        return;
+    }
+
+    // Perform calculations
+    var vDistanceTraveled = distanceTraveled.value / 100.0;
+    var carFuelPrice = carFuel.value * priceOfGasoline.value;
+    var fuelConsumption = vDistanceTraveled * carFuelPrice;
+
+    // Display the result
+    document.getElementById("fuelConsumption").innerHTML = "Amount of fuel consumption in Jordanian dinars JD: " + fuelConsumption + " JD.";
+}*/
